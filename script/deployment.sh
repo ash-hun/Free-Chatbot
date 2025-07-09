7f269a4f31d2e2c85544b2c98db2cc92abb1b1dc
ash-hun <diadiahun0902@gmail.com>
2025-07-09 21:25:49 +0900

    update: deployment script

--- script/deployment.sh
+++ script/deployment.sh
@@ -1,3 +1,3 @@
 cd environments/ && docker compose down --rmi all
-cd ../ && docker build -t free_chatbot:latest -f environments/Dockerfile .
+cd ../ && docker build --platform linux/amd64 -t free_chatbot:latest -f environments/Dockerfile .
 cd environments/ && docker compose up
